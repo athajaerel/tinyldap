fe1781c1cbc4acc265e3271b6eee7eb5e56d7d48
Adam Richardson <adam.richardson@cgi.com>
2024-09-25 16:07:49 +0100

    Add tags and push target

--- common.mk
+++ common.mk
@@ -1,3 +1,8 @@
+REMOTE := "ghcr.io"
+IMAGE := $(shell echo ${REMOTE})/dreamtrack-net/tinyldap
+NOW := $(shell date -Iseconds | cut -c 1-19 | tr ' :' '__')Z
+TAG_DT := $(shell echo ${NOW} | cut -c 1-10)
+
 .PHONY: tinyldap
 tinyldap: BUILD_FLAGS := \
 		--http-proxy=false \
@@ -7,6 +12,9 @@ tinyldap: BUILD_FLAGS := \
 		--format=oci \
 		--build-arg=PKGS="${pkgs} ${temp_pkgs}" \
 		--build-arg=STAGE_PKGS="${pkgs}" \
+		--tag="$(IMAGE):latest" \
+		--tag="$(IMAGE):$(TAG_DT)" \
+		--tag="$(IMAGE):$(NOW)" \
 		--log-level=info
 
 tinyldap:
@@ -21,4 +29,11 @@ clean: prune
 .PHONY: prune
 prune:
 	podman rmi -a || true
+
+.PHONY: push
+push: tinyldap
+	no_proxy=* NO_PROXY=* podman login $(REMOTE)
+	no_proxy=* NO_PROXY=* podman push $(IMAGE):latest
+	no_proxy=* NO_PROXY=* podman push $(IMAGE):$(TAG_DT)
+	no_proxy=* NO_PROXY=* podman push $(IMAGE):$(NOW)
 
