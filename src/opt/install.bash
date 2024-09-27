3f4734a659d24aaee46cd9af46a23ed899c60619
Adam Richardson <adam.richardson@cgi.com>
2024-09-27 16:15:57 +0100

    Update install script

--- src/opt/install.bash
+++ src/opt/install.bash
@@ -3,3 +3,5 @@ set -exuo pipefail
 
 ldapmodify -c -Q -Y EXTERNAL -H ldapi:/// -f /opt/config.ldif
 ldapmodify -c -Q -Y EXTERNAL -H ldapi:/// -f /opt/tls.ldif
+ldapmodify -c -Q -Y EXTERNAL -H ldapi:/// -f /opt/basic_infra.ldif
+ldapmodify -c -Q -Y EXTERNAL -H ldapi:/// -f /opt/first_user.ldif
